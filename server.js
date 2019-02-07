415d56373aba58186328249dbe0a61f0a2e4784b
Brian Nickila <brian.nickila@gmail.com>
2019-02-06 23:39:41 -0600

    Got the saved articles to show up on the saved page.

--- server.js
+++ server.js
@@ -22,10 +22,21 @@ app.set('index', __dirname + '/views');
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-var results = [];
-// Start routes here...
+//var results = [];
 
+// Start routes here...
 app.get("/", function (req, res) {
+    allArticles = [];
+
+    db.Article.find({ saved: false }, function (err, result) {
+        if (err) throw err;
+        allArticles.push(result)
+        console.log(allArticles);
+        res.render("index", { result })
+    })
+
+});
+app.get("/newscrape", function (req, res) {
     axios.get("https://www.nytimes.com/").then(function (response) {
         var $ = cheerio.load(response.data)
         $("h2 span").each(function (i, element) {
@@ -52,96 +63,35 @@ app.get("/", function (req, res) {
             .catch(function (err) {
                 console.log(err);
             })
-        app.get("/", function (req, res) {
-            res.render("index")
-        })
+        // app.get("/", function (req, res) {
+        //     res.render("index")
+        // })
     })
 });
 
 app.put("/update/:id", function (req, res) {
-// console.log(req.params);
-    console.log("##########################################");
-    console.log(" ");
-    console.log(" ")
-    console.log("##########################################");
-    console.log(req.params);
-    db.Article.updateOne({ _id: req.params.id }, { $set: { saved: true } }, function(err, result) {
-        if (result.changedRows == 0) { 
-        return res.status(404).end();
-    } else {
-        res.status(200).end();
-    }
+    db.Article.updateOne({ _id: req.params.id }, { $set: { saved: true } }, function (err, result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
 });
-    // .then(function (dbSaved) {
-    //     console.log(dbSaved);
 
+
+
+app.get("/saved", function (req, res) {
+    var savedArticles = [];
+    db.Article.find({ saved: true }, function (err, saved) {
+        if (err) throw err;
+        savedArticles.push(saved)
+        res.render("saved", { saved })
+    })
+       
+    // app.get("/", function(req, res) {
+    //     res.render("index");
     // })
-    // .catch(function (err) {
-    //     console.log(err);
-    // })
-
-    // db.Article.updateOne({ _id: req.params.id }, { $set: { saved: true } }, function (err, result) {
-    //     if (result.changedRows == 0) {
-    //         return res.status(404).end();
-    //     } else {
-    //         res.status(200).end();
-    //     }
-
-    // });
-    // var data = req.body;
-    // console.log(data);
-    // console.log(req.params);
-    // $.ajax({
-    //     type: "PUT",
-    //     url: "/update/" + id,
-    //     data: {saved: change}
-    // }).then(function(response) {
-    //     console.log(response);
-    //     window.location.replace("/");
-    // }).fail(function(response) {
-    //     console.log("Not working")
-    // })
-
-    // router.put("/api/burgers/:id", function (req, res) {
-    //     console.log(req.params)
-
-    //     var condition = "id=" + req.params.id;
-    //     burger.update({
-    //         devoured: req.body.devoured
-    //     }, condition, function (result) {
-    //         if (result.changedRows == 0) {
-    //             return res.status(404).end();
-    //         } else {
-    //             res.status(200).end();
-    //         }
-
-    //     });
-
-    // });
-    // db.Article.updateOne({ _id: req.params.id }, { $set: { saved: true } }, function (err, result) {
-    //     if (err) {
-    //         console.log(err);
-
-    //     }
-    //     res.send("Saved successfully!");
-    // })
-
-    // console.log(req);
-    // console.log(res);
-    // var savedArticle = {
-    //     saved: true
-    // }
-
-   
-    // db.Article.create(results)
-    //     .then(function (dbArticle) {
-    //         console.log(dbArticle);
-    //     })
-    //     .catch(function (err) {
-    //         console.log(err);
-    //     })
-
-
 })
 
 
